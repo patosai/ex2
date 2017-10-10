bc5b511a81a67752c9e540f08d0ff3ec450679e4
Patrick Tsai <penguin@patosai.com>
2017-10-09 21:33:09 -0400

    Remote host wakeup

--- include/keymap_left.h
+++ include/keymap_left.h
@@ -14,13 +14,13 @@ const keycode_t KEYMAP[KEYMAP_LAYERS][MATRIX_ROWS][MATRIX_COLS] PROGMEM = {
     { KC_LSHIFT,    KC_Z,     KC_X,     KC_C,    KC_V,      KC_B,      KC_UP },
     { KC_LCTRL,     KC_LGUI,  KC_LALT,  KC_MUTE, KC_DELETE, KC_BSPACE, KC_DOWN }
   },
-  // 1 - to be filled in
+  // 1 - gaming
   {
-    { KC_ESCAPE,  KC_NONE, KC_NONE,  KC_NONE, KC_NONE,     KC_NONE,   KC_LAYER_0 },
-    { KC_NONE,    KC_NONE, KC_NONE,  KC_NONE, KC_NONE,     KC_NONE,   KC_LAYER_1 },
-    { KC_NONE,    KC_NONE, KC_NONE,  KC_NONE, KC_NONE,     KC_NONE,   KC_NONE },
-    { KC_NONE,    KC_NONE, KC_NONE,  KC_NONE, KC_NONE,     KC_NONE,   KC_NONE },
-    { KC_NONE,    KC_NONE, KC_NONE,  KC_NONE, KC_DELETE,   KC_BSPACE, KC_NONE },
+    { KC_ESCAPE,    KC_1,     KC_2,     KC_3,    KC_4,      KC_5,      KC_LAYER_0 },
+    { KC_TAB,       KC_Q,     KC_W,     KC_E,    KC_R,      KC_T,      KC_LAYER_1 },
+    { KC_CAPSLOCK,  KC_A,     KC_S,     KC_D,    KC_F,      KC_G,      KC_LBRACKET },
+    { KC_LSHIFT,    KC_Z,     KC_X,     KC_C,    KC_V,      KC_B,      KC_UP },
+    { KC_LCTRL,     KC_LGUI,  KC_LALT,  KC_MUTE, KC_TAB,    KC_SPACE,  KC_DOWN }
   }
 };
 
